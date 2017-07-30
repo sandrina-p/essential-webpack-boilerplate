84a20b7c114339047f39af7e4962870e876fee15
sandrina-p <sandrin4pereir4@gmail.com>
2017-07-30 01:39:44 +0100

    remove postcss-extend

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     plugins: [
         require('postcss-import'),
-        require('postcss-extend'),
         require('postcss-cssnext')({
             features: {
                 rem: false, // it will not add fallback from rem to px
