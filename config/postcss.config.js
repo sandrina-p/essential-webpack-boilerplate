8f95b711a9bc0850ebacb2e2b8531f445b03d528
sandrina-p <sandrin4pereir4@gmail.com>
2017-07-30 01:32:32 +0100

    add postcss-nested

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     plugins: [
         require('postcss-import'),
+        require('postcss-extend'),
         require('postcss-cssnext')({
             features: {
                 rem: false, // it will not add fallback from rem to px
